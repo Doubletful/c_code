5d8accdea50eb003d56fb44a025ed53a13c4b3c0
Doubletful <15584481120m@sina.cn>
2026-05-30 20:33:10 +0800

    Update practices22.c

--- practices22.c
+++ practices22.c
@@ -1,1 +1,24 @@
-//367. 有效的完全平方数
+//LeetCode    367.有效的完全平方数
+
+/*
+//方法一：二分查找
+bool isPerfectSquare(int num) {
+    int left = 0, right = num, ans = -1;
+    while (left <= right)
+    {
+        long mid = left + (right - left) / 2;
+        if (mid * mid == num)
+            return true;
+        else if (mid * mid > num)
+            right = mid - 1;
+        else
+            left = mid + 1;
+    }
+    return false;
+}
+
+//方法二：数学
+//数学知识：连续奇数之和等于平方数
+//公式：n*2 = 1 + 3 + 5 + ⋯ + (2n − 1)
+
+*/
